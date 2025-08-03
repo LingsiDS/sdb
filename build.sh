0f0fe2e3c00f07a43f263d3601f2604c09296480
lingsi <1053781247@qq.com>
2025-08-03 15:21:14 +0800

    fix compile bugs

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir -p ./build
 cd build
 # cmake .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake --fresh
+cmake .. -DCMAKE_TOOLCHAIN_FILE=/home/lingsi/install/vcpkg/scripts/buildsystems/vcpkg.cmake  --fresh
 make -j8
 cd ..
